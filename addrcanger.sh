a42a6db22abff0d186aad1a324736baac5ebf770
Alex Medved <alex@playtagon.com>
2017-07-28 12:44:12 +0300

    Modified the script for autoupgrading thr git container <gitlab/gitupd.sh>, Modified <addrcanger.sh> with adding check what is the strind is not null for auto changing ip of proxy on nginx

--- addrcanger.sh
+++ addrcanger.sh
@@ -6,21 +6,27 @@ CFBAMBOO=/etc/nginx/conf.d/bambo_http.conf
 BAMBOO=`docker inspect -f='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bamboo`
 GIT=`docker inspect -f='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' git`
 JEN=`docker inspect -f='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jenkins`
-#echo "for jenkins"
-sed -i -- 's/172.*.*.*/'"${BAMBOO}"':8085;/g' $CFBAMBOO
 
-#echo "for git"
-sed -i -- 's/172.*.*.*/'"${GIT}"':80;/g' $CFGIT
-
-#echo "for jenkins"
-sed -i -- 's/172.*.*.*/'"${JEN}"':8080;/g' $CFJEN
+# Condition for BAMBOO
+if [ -z "$BAMBOO" ]
+then
+  echo "$BAMBOO is null."
+else
+  sed -i -- 's/172.*.*.*/'"${BAMBOO}"':8085;/g' $CFBAMBOO
+fi
+# check for GIT
+if [ -z "$GIT" ]
+then
+  echo "$GIT is null."
+else
+  sed -i -- 's/172.*.*.*/'"${GIT}"':8085;/g' $CFGIT
+fi
+# Condition for Jenkins
+if [ -z "$JEN" ]
+then
+  echo "$JEN is null."
+else
+  sed -i -- 's/172.*.*.*/'"${JEN}"':8085;/g' $CFJEN
+fi
 #grep proxy_pass {$CFBAMBOO,$CFGIT,$CFJEN,}
 systemctl reload-or-restart nginx
-
-#[root@doc jenkins]# docker inspect -f='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' jenkins
-
-#[root@doc jenkins]# docker inspect -f='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' git
-
-#[root@doc jenkins]# docker inspect -f='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' bamboo
-
-
